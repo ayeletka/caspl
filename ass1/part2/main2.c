fecfbc590e846890cb5dc8c1f3f52b498a069660
Barak Yaari <barakyaari@gmail.com>
2016-04-12 14:50:09 +0300

    test

--- ass1/part2/main2.c
+++ ass1/part2/main2.c
@@ -6,20 +6,44 @@
 
 int main(int argc, char** argv)
 {
-  unsigned long long  x_num;
+  long long *x_num;
+  long long allocated[1];
+  x_num = allocated;
+  int i;
+  for(i = 0; i<16; i++){
+    ((char*)x_num)[i] = 0;
+  }
   int numOfRounds;
-	
+  char str[8];
+  for(i = 0; i<8; i++){
+    str[i] = 0;
+  }
+
   fflush(stdout);
   printf("%s\n","please insert a num in hexadecimal:" );
-  fgets(x_num, 64, stdin);
-  printf("Got the number: %llx\n", x_num);
+  
+  fgets(str, 8, stdin);
+  printf("Got the number: %s\n", str);
+  for(i=0; i<8; i++){
+    if(str[i]<':'&&str[i]>'/')
+      str[i]=str[i]-48;
+    else if (str[i]<='F'&&str[i]>='A') str[i]=str[i]-55;
+    ((char*)(x_num))[i]=str[i];
+  }
 
- fflush(stdout);
+  printf("Got the number: %X\n", *x_num);
+  printf("Number value: %lld\n", *x_num);
+
+
+
+
+  fflush(stdout);
   printf("%s\n","please insert a num of rounds:" );
-  fgets(numOfRounds, 32, stdin);
+  scanf("%d", &numOfRounds);
+  
   printf("Got the number: %d\n", numOfRounds);
 
-  //my_func(buf);
+  //calc_func(buf);
 
   return 0;
 }
